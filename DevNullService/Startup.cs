59784ffb414c71bbef224a84528f8b4d56e515d3
Stephen Riley <jstephen.riley@yahoo.com>
2019-10-24 22:59:44 -0700

    remove SuppressMessage for 1822 in Startup
    turns out Configure and ConfigureServices can be static

--- DevNullService/Startup.cs
+++ DevNullService/Startup.cs
@@ -21,7 +21,7 @@ namespace DevNullService
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddRazorPages();
@@ -39,7 +39,7 @@ namespace DevNullService
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
